2adff15bc0c9c07cf80bf8add47a9c47e2afa157
mariacssilva <mariaclarasilvamg@gmail.com>
2025-10-25 15:09:55 -0300

    Acrescentando arquivo avaliacao.hpp

---
+++ avaliacao.hpp
@@ -0,0 +1,27 @@
+#ifndef AVALIACAO_HPP
+#define AVALIACAO_HPP
+
+#include <string>
+#include <fstream>
+#include "disciplina.hpp"
+#include "turma.hpp"
+#include "usuario.hpp"
+
+class Avaliacao {
+private:
+    std::string _tipo;
+    Disciplina _disciplina;
+    Turma _turma;
+    Usuario _usuario;
+    
+
+
+public:
+
+};
+
+
+
+
+
+#endif
