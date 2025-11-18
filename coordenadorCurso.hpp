c767c5b4cc3d829afa566cbebce30e6b461d96f5
Bianca Solleny <biancasolleny141@gmail.com>
2025-11-18 18:12:18 -0300

    alteracoes.hpp

--- coordenadorCurso.hpp
+++ coordenadorCurso.hpp
@@ -1,17 +1,15 @@
 #ifndef COORDCURSO_HPP
 #define COORDCURSO_HPP
 
-#include "Usuario.hpp"
+#include "usuario.hpp"
 
-// Coordenador de curso é o administrador do sistema.
-// Cadastra usuários e disciplinas.
+// Coordenador de curso e' o administrador do sistema
+// Cadastra usuarios e disciplinas
 class CoordCurso : public Usuario {
 public:
     CoordCurso() = default;
-    CoordCurso(int id, const std::string &nome,
-               const std::string &email,
-               const std::string &senha)
-        : Usuario(id, nome, email, senha, "COORD_CURSO") {}
+    CoordCurso(int id, const std::string &nome, const std::string &email, const std::string &senha);
 };
 
 #endif
+
