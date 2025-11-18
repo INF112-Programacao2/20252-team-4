8b97b9103bb9514e1d8d216400f0240aca741ecf
mariacssilva <mariaclarasilvamg@gmail.com>
2025-11-18 08:43:22 -0300

    Criando arquivos .cpp  das classes CoordenadorDisciplina e Disciplina e finalizando construtores.

---
+++ coordenadorDisciplina.cpp
@@ -0,0 +1,6 @@
+#include "coordenadorDisciplina.hpp"
+
+CoordenadorDisciplina::CoordenadorDisciplina() = default;
+
+CoordenadorDisciplina::CoordenadorDisciplina(int id, const std::string& nome, const std::string& email, const std::string& senha) :
+    Usuario(id, nome, email, senha, "COORD_DISC") {}
---
+++ disciplina.cpp
@@ -0,0 +1,29 @@
+#include "disciplina.hpp"
+
+Disciplina::Disciplina() = default;
+
+Disciplina::Disciplina(int id, const std::string& codigo, const std::string& nome, int professorId, int coordenadorDiscId) :
+        _id(id), _codigo(codigo), _nome(nome), _professorId(professorId), _coordenadorDiscId(coordenadorDiscId) {}
+
+int Disciplina::getId() const {
+    return _id; 
+}
+
+std::string Disciplina::getCodigo() const {
+    return  _codigo;
+}
+
+std::string Disciplina::getNome() const {
+    return _nome;
+}
+
+int Disciplina::getProfessorId() const {
+    return _professorId;
+}
+
+int Disciplina::getCoordenadorDiscId() const {
+    return _coordenadorDiscId;
+}
+
+
+
