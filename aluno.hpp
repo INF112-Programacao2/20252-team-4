57870549fc1d1cd59995cedae6b89a61c50e9d7b
Bianca Solleny <biancasolleny141@gmail.com>
2025-11-18 18:10:15 -0300

    mudancas aluno.hpp

--- aluno.hpp
+++ aluno.hpp
@@ -1,17 +1,16 @@
 #ifndef ALUNO_HPP
 #define ALUNO_HPP
 
-#include "Usuario.hpp"
+#include "usuario.hpp"
+#include <string>
 
-// Representa um aluno no sistema.
-// Não tem permissões administrativas.
-class Aluno : public Usuario {
-public:
+// Representa um aluno no sistema
+// Nao tem permissões administrativas
+class Aluno : public Usuario { //herda da classe usuario e so' tem direito a login
+
+  public:
     Aluno() = default;
-    Aluno(int id, const std::string &nome,
-          const std::string &email,
-          const std::string &senha)
-        : Usuario(id, nome, email, senha, "ALUNO") {}
+    Aluno(int id, const std::string &nome, const std::string &email, const std::string &senha);
 };
 
 #endif
